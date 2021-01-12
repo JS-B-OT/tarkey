b9642bdbf5a831310227b2fbca4365d4a0e370dd
JS-B-OT <74894397+JS-B-OT@users.noreply.github.com>
2021-01-12 09:38:17 +0700

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,20 @@
+const donasi = () => {
+	return `
+
+┏━━━━━━━━━━━━━━━━━━━━
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ Donasi Seikhlasnya❉⊰━━✿
+┃  
+┣━⊱ *DANA*
+┣⊱ 08Soon
+┣━⊱ *PULSA*
+┣⊱ 0858-0944-0506
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY JDR*
+┗━━━━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.donasi = donasi
